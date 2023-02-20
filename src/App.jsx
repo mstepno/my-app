fa896616505c37cfdcde95f81f9dd76b24fa79ad
mstepno <121028153+mstepno@users.noreply.github.com>
2023-02-20 22:02:14 +0200

    react intial

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Bebras from './Components/003/Bebras';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+ 
+      <Bebras />
+        <Bebras></Bebras>
+
+      </header>
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/Bebras.jsx
@@ -0,0 +1,12 @@
+function Bebras() {
+
+    return (
+        <>
+            <h1><i>Bebras</i></h1>
+            <h2>Barsukas</h2>
+        </>
+    );
+
+
+}
+export default Bebras;
